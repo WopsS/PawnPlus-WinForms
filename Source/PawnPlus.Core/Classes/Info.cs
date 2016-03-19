6d0a9717e495a942cf06d3ac7e2ecce811dbbaee
Nils Peters <septagon@blackace.org>
2016-03-19 16:08:08 +0100

    Code Cleanup

--- Source/PawnPlus.Core/Classes/Info.cs
+++ Source/PawnPlus.Core/Classes/Info.cs
@@ -1,15 +1,7 @@
-﻿
-using System.Collections.Generic;
-
-namespace PawnPlus.Core.Classes
+﻿namespace PawnPlus.Core.Classes
 {
-    enum settingslist
-    {
-        lastWindowState = 1
-    }
     class Info
     {
-        public const string config = "settings.xml";
-        public static List<string> Settings = new List<string>();
+        
     }
 }
